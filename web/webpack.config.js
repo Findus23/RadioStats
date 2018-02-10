c8fa6f0e8212f9bc6eeeb8f5ef88b9467980d72c
Lukas Winkler <l.winkler23@mailbox.org>
2018-02-10 17:51:11 +0100

    update dependencies

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -104,7 +104,7 @@ if (process.env.NODE_ENV === 'production') {
     module.exports.devtool = '#source-map';
     // http://vue-loader.vuejs.org/en/workflow/production.html
     module.exports.plugins = (module.exports.plugins || []).concat([
-        new CleanWebpackPlugin("dist"),
+        new CleanWebpackPlugin("../dist"),
         new webpack.HashedModuleIdsPlugin({
             hashFunction: 'sha256',
             hashDigest: 'hex',
