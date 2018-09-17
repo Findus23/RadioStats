19f069ea5e6ba5eb6a01114389fcac3f64e07ddb
Lukas Winkler <git@lw1.at>
2018-09-17 20:59:28 +0200

    add sentry

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 # Blog configuration values.
 
+import logging
+
 from flask import Flask
 from playhouse.flask_utils import FlaskDB
 from playhouse.pool import PooledMySQLDatabase
+from raven.contrib.flask import Sentry
 
 import config
 
 DATABASE = PooledMySQLDatabase("radio", **config.db)
 
-
 # Create a Flask WSGI app and configure it using values from the module.
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+if config.sentryDSN:
+    sentry = Sentry(app, dsn=config.sentryDSN, logging=True, level=logging.WARNING)
 
 flask_db = FlaskDB(app)
 
