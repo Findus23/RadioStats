2e1d1f173b3645bca1574aa886fc7c5b5b0d9c60
Lukas Winkler <git@lw1.at>
2018-09-21 21:06:03 +0200

    add sentry

--- web/main.js
+++ web/main.js
@@ -3,9 +3,15 @@ import VueHead from 'vue-head';
 import App from './App.vue';
 import router from './routes';
 import MatomoTracker from './MatomoTracker';
+import {init, Integrations} from '@sentry/browser';
+
+init({
+    dsn: 'https://91af780499634f98a17afe160c6ace89@sentry.lw1.at/12',
+    integrations: [new Integrations.Vue({Vue})]
+});
 
 Vue.use(VueHead);
-
+throw new Error()
 let matomo = new MatomoTracker;
 matomo.init();
 
